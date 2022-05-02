36ef2cd08adb5867248934c4cc78756963fab070
Андрей Логинов <andreyloginov1992@gmail.com>
2022-05-02 21:29:13 +0300

    Batch means variance estimation fix(or not)

--- MCMC.R
+++ MCMC.R
@@ -1,5 +1,5 @@
 MHScoreSample <- function(N, target.str, alphabet.len, gamma,
-                         start.str = sample(0:(alphabet.len - 1), str.len, replace = T)){
+                          start.str = sample(0:(alphabet.len - 1), str.len, replace = T)){
   str.len = length(target.str)
   curr.str = start.str
   pos.change = sample(1:str.len, N, replace = T)
@@ -7,13 +7,19 @@ MHScoreSample <- function(N, target.str, alphabet.len, gamma,
   unif01 <- runif(N)
   
   return(MHScoreSampling(N, 
-                          as.integer(target.str),
-                          as.integer(alphabet.len),
-                          gamma,
-                          as.integer(start.str),
-                          as.integer(pos.change),
-                          as.integer(shift),
-                          unif01))
+                         as.integer(target.str),
+                         as.integer(alphabet.len),
+                         gamma,
+                         as.integer(start.str),
+                         as.integer(pos.change),
+                         as.integer(shift),
+                         unif01))
+}
+
+N.batches <- function(N) {
+  u <- floor(sqrt(N))
+  while (N %% u != 0) u <- u - 1
+  return(u)
 }
 
 MH.UC.Rcpp <- function(N, target.str, thr, alphabet.len, gamma, batch = F, 
@@ -25,11 +31,14 @@ MH.UC.Rcpp <- function(N, target.str, thr, alphabet.len, gamma, batch = F,
   weights = vapply(scores, function(x)  exp(-x * gamma), numeric(1))
   h = as.numeric(scores >= thr)
   if (batch){
-    u = trunc(sqrt(N))
-    v = u
+    u = N.batches(N)
+    v = N / u
     Y = vapply(1:u, 
-               function(x) mean(h[((x-1)*v):(x*v-1)] * weights[((x-1)*v):(x*v-1)]) / mean(weights),
+               function(n) mean(h[((n - 1) * v + 1) : (n * v)] * weights[((n - 1) * v + 1) : (n * v)]) / mean(weights),
                numeric(1))
+    #Y = vapply(1:u, 
+    #           function(x) mean(h[((x-1)*v):(x*v-1)] * weights[((x-1)*v):(x*v-1)]) / mean(weights),
+    #           numeric(1))
     p_est = sum(h*weights) / sum(weights)
     var_est = (v / (u - 1)) * sum((Y - p_est)^2) / N
     return(list(p = p_est, var = var_est))
@@ -38,5 +47,3 @@ MH.UC.Rcpp <- function(N, target.str, thr, alphabet.len, gamma, batch = F,
     return(sum(h * weights) / sum(weights))
   }
 }
-
-MH.UC.Rcpp(10^5, rep(0, 20), 15, 4, 1)
